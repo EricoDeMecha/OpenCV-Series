42c987f6c166fe3537dfd41c07a54f6bd70bc042
EricoDeMecha <techcider4134@gmail.com>
2020-10-06 17:01:14 +0300

    Image annotations 1

---
+++ ImageAnnotation/cmake-build-debug/CMakeFiles/ImageAnnotation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageAnnotation.dir/main.cpp.o"
+  "ImageAnnotation"
+  "ImageAnnotation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageAnnotation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PetProject/cartoonifier/cmake-build-debug/CMakeFiles/cartoonifier.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cartoonifier.dir/main.cpp.o"
+  "cartoonifier"
+  "cartoonifier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cartoonifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
